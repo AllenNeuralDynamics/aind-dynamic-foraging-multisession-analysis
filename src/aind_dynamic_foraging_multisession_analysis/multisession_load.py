eb52a224d9237afcaf5df73de3d61b6ba54f9227
Alex Piet <alexpiet@gmail.com>
2025-09-02 15:43:29 -0700

    dev

--- src/aind_dynamic_foraging_multisession_analysis/multisession_load.py
+++ src/aind_dynamic_foraging_multisession_analysis/multisession_load.py
@@ -31,7 +31,7 @@ def make_multisession_trials_df(nwb_list):
         try:
             nwb = nu.load_nwb_from_filename(n)
             nwb.df_trials = nu.create_df_trials(nwb, verbose=False)
-            nwb.df_events = nu.create_events_df(nwb, verbose=False)
+            nwb.df_events = nu.create_df_events(nwb, verbose=False)
             nwb.df_licks = a.annotate_licks(nwb)
             nwb.df_trials = tm.compute_trial_metrics(nwb)
             nwb.df_trials = add_side_bias(nwb)
